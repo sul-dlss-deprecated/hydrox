b2b06c5b2f9d9c76035b2f2cd701126dfa966c50
Aaron Collier <aaron.collier@stanford.edu>
2017-10-26 13:31:01 -0700

    Remove empty line per rubocop

--- app/services/suri/druid.rb
+++ app/services/suri/druid.rb
@@ -4,7 +4,6 @@ module Suri
   # Mix this module into generated work types in order to have IDs minted
   # as a druid.
   module Druid
-
     ## This overrides the default behavior, which is to ask Fedora for an id
     # @see ActiveFedora::Persistence.assign_id
     def assign_id
