430a0c943800d3e35bc118a4e2e1e6bb80bef57b
Erin Fahy <efahy@stanford.edu>
2017-11-29 11:50:18 -0800

    Adds active_elastic_job gem and config

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,5 @@
+# :default_queue matches a preexisting SQS queue in AWS
+# per https://github.com/tawan/active-elastic-job#usage
 class ApplicationJob < ActiveJob::Base
+  queue_as :default_queue
 end
