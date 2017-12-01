65b09709e2331ef48cea62d2720bec76c6b853a5
Justin Coyne <jcoyne@justincoyne.com>
2017-12-01 10:46:50 -0800

    Stop using deprecated method human_readable_type=

--- app/models/work.rb
+++ app/models/work.rb
@@ -9,8 +9,6 @@ class Work < ActiveFedora::Base
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
 
-  self.human_readable_type = 'Work'
-
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
