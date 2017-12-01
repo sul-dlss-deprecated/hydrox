0e2d08814113d1899b4a5f629ced6408f20737bc
Justin Coyne <jcoyne@justincoyne.com>
2017-12-01 10:32:07 -0800

    Make the create work test actually create a work.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,5 @@ module Hydrox
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.active_job.queue_adapter = :active_elastic_job
   end
 end
