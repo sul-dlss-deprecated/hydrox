cd075e5e2b867073a78a21a2a6a28405aef3895d
Aaron Collier <aaron.collier@stanford.edu>
2017-12-07 16:13:58 -0800

    send env to callbacks

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def shibboleth
-    user = resource_class.from_omniauth(request.env["omniauth.auth"])
+    user = resource_class.from_omniauth(request.env)
     sign_in_and_redirect user
   end
 end
